9b2545059435d6102bd956a2304cec6141ca7edf
monica2020qthings-droid <monica2020qthings@outlook.com>
2025-12-03 18:16:36 -0600

    Avance: rutas y controladores de autenticación + ajustes en app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // app.js
 import express from 'express';
 import cors from 'cors';
+import authRoutes from './src/routes/auth.routes.js';
+
 
 const app = express();
 
@@ -9,6 +11,8 @@ app.use(express.json());
 
 // Ruta de prueba para verificar que la API responde
 app.get('/api/health', (req, res) => {
+  // Rutas de autenticación
+app.use('/api/auth', authRoutes);
   res.status(200).json({ message: 'API funcionando!' });
 });
 
---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+// src/controllers/auth.controller.js
+
+export const register = (req, res) => {
+  res.send("Register pendiente por implementar");
+};
+
+export const login = (req, res) => {
+  res.send("Login pendiente por implementar");
+};
